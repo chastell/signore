e2a0228493d11c5487b01ef4fcd4f6de9d03001e
Piotr Szotkowski <shot@hot.pl>
2009-07-16 11:50:26 +0200

    a trivial, inefficient Signature.find_random_by_labels

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -2,4 +2,8 @@ module Signore class Signature < Sequel::Model
 
   many_to_many :labels, :class => 'Signore::Label'
 
+  def self.find_random_by_labels labels
+    labels.map { |label| Label[:name => label].signatures }.inject(:&).sort_by { rand }.first
+  end
+
 end end
---
+++ spec/signore/signature_spec.rb
@@ -0,0 +1,17 @@
+inside_connection do
+module Signore describe Signature do
+
+  it 'should return a proper random signature based on the provided labels' do
+    Signore.db.transaction do
+      Signature.find_random_by_labels(['programming']).text.should == '// sometimes I believe compiler ignores all my comments'
+      Signature.find_random_by_labels(['programming', 'tech']).text.should == '// sometimes I believe compiler ignores all my comments'
+      srand 1981
+      Signature.find_random_by_labels(['tech']).text.should == '// sometimes I believe compiler ignores all my comments'
+      srand 1979
+      Signature.find_random_by_labels(['tech']).text.force_encoding('UTF-8').should == 'You do have to be mad to work here, but it doesn’t help.'
+      raise Sequel::Rollback
+    end
+  end
+
+end end
+end
