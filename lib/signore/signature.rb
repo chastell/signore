9e09cc36375c9026cfb8b5e13f8f032ce5add61c
Piotr Szotkowski <shot@hot.pl>
2009-07-16 12:40:26 +0200

    make Signature.find_random_by_labels work with empty label set

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -3,6 +3,7 @@ module Signore class Signature < Sequel::Model
   many_to_many :labels, :class => 'Signore::Label'
 
   def self.find_random_by_labels labels
+    return all.sort_by { rand }.first if labels.empty?
     labels.map { |label| Label[:name => label].signatures }.inject(:&).sort_by { rand }.first
   end
 
--- spec/signore/signature_spec.rb
+++ spec/signore/signature_spec.rb
@@ -3,6 +3,7 @@ module Signore describe Signature do
 
   it 'should return a proper random signature based on the provided labels' do
     Signore.db.transaction do
+      lambda { Signature.find_random_by_labels([]) }.should_not raise_error
       Signature.find_random_by_labels(['programming']).text.should == '// sometimes I believe compiler ignores all my comments'
       Signature.find_random_by_labels(['programming', 'tech']).text.should == '// sometimes I believe compiler ignores all my comments'
       srand 1981
