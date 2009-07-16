4edee865f5945bcfaba39587874578ca09bbbb18
Piotr Szotkowski <shot@hot.pl>
2009-07-16 11:48:20 +0200

    a helper method that makes sure Signore is connected to a db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,16 @@
 require 'tmpdir'
 
 require 'lib/signore'
+
+def inside_connection
+  if Signore.connected?
+    yield
+  else
+    file = "#{Dir.tmpdir}/#{rand}/signore.db"
+    FileUtils.mkpath File.dirname file
+    FileUtils.copy 'spec/fixtures/example.db', file
+    Signore.connect file
+    yield
+    FileUtils.rmtree File.dirname file
+  end
+end
