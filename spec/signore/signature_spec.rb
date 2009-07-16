c7bd016979788b71959ccc453cc4f1b2485f2f4d
Piotr Szotkowski <shot@hot.pl>
2009-07-16 12:07:26 +0200

    make the Signature.find_random_by_labels spec reset the random generator

--- spec/signore/signature_spec.rb
+++ spec/signore/signature_spec.rb
@@ -9,6 +9,7 @@ module Signore describe Signature do
       Signature.find_random_by_labels(['tech']).text.should == '// sometimes I believe compiler ignores all my comments'
       srand 1979
       Signature.find_random_by_labels(['tech']).text.force_encoding('UTF-8').should == 'You do have to be mad to work here, but it doesn’t help.'
+      srand
       raise Sequel::Rollback
     end
   end
