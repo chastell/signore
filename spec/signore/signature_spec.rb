72822688547b9465e1c0c491d62b30f3165f6674
Piotr Szotkowski <shot@hot.pl>
2009-07-18 16:48:04 +0200

    if this changes the db, then we’d rather know about the fact

--- spec/signore/signature_spec.rb
+++ spec/signore/signature_spec.rb
@@ -1,18 +1,15 @@
 module Signore describe Signature do
 
   it 'should return a proper random signature based on the provided labels' do
-    Signore.db.transaction do
-      lambda { Signature.find_random_by_labels([]) }.should_not raise_error
-      lambda { Signature.find_random_by_labels(['foo']) }.should_not raise_error
-      Signature.find_random_by_labels(['programming']).text.should == '// sometimes I believe compiler ignores all my comments'
-      Signature.find_random_by_labels(['programming', 'tech']).text.should == '// sometimes I believe compiler ignores all my comments'
-      srand 1981
-      Signature.find_random_by_labels(['tech']).text.should == '// sometimes I believe compiler ignores all my comments'
-      srand 1979
-      Signature.find_random_by_labels(['tech']).text.force_encoding('UTF-8').should == 'Bruce Schneier knows Alice and Bob’s shared secret.'
-      srand
-      raise Sequel::Rollback
-    end
+    lambda { Signature.find_random_by_labels([]) }.should_not raise_error
+    lambda { Signature.find_random_by_labels(['foo']) }.should_not raise_error
+    Signature.find_random_by_labels(['programming']).text.should == '// sometimes I believe compiler ignores all my comments'
+    Signature.find_random_by_labels(['programming', 'tech']).text.should == '// sometimes I believe compiler ignores all my comments'
+    srand 1981
+    Signature.find_random_by_labels(['tech']).text.should == '// sometimes I believe compiler ignores all my comments'
+    srand 1979
+    Signature.find_random_by_labels(['tech']).text.force_encoding('UTF-8').should == 'Bruce Schneier knows Alice and Bob’s shared secret.'
+    srand
   end
 
   it 'should properly display signatures with (and without) author/source' do
