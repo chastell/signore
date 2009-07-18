ecdddaba1924276f59dec763a3a87afe9ee047cd
Piotr Szotkowski <shot@hot.pl>
2009-07-18 17:56:15 +0200

    add an in_transaction method and use it in Signature specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,10 @@ require 'tmpdir'
 require 'lib/signore'
 
 Signore.connect 'spec/fixtures/example.db'
+
+def in_transaction
+  Signore.db.transaction do
+    yield
+    raise Sequel::Rollback
+  end
+end
