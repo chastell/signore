0c4765987fc4b0a38e3769c0a9995a34ab458de1
Piotr Szotkowski <shot@hot.pl>
2009-08-07 17:36:47 +0200

    refactor Signature#wrap by splitting out Signature#wrap_line

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -54,18 +54,23 @@ module Signore class Signature < Sequel::Model
   end
 
   def wrap lines
+    last_line = lines.split("\n").last
     lines.split("\n").map do |line|
-      best_wrap = line.gsub /(.{1,80})( |$\n?)/, "\\1\n"
-      max_height = best_wrap.count "\n"
-      79.downto 1 do |size|
-        new_wrap = line.gsub /(.{1,#{size}})( |$\n?)/, "\\1\n"
-        lengths = new_wrap.split("\n").map(&:size)
-        break if has_meta? and line == lines.split("\n").last and lengths.last == lengths.max and lengths.count(lengths.max) == 1
-        break if new_wrap.count("\n") > max_height
-        best_wrap = new_wrap
-      end
-      best_wrap.chomp
+      wrap_line line, line == last_line
     end.join "\n"
+  end
+
+  def wrap_line line, is_last
+    best_wrap = line.gsub /(.{1,80})( |$\n?)/, "\\1\n"
+    max_height = best_wrap.count "\n"
+    79.downto 1 do |size|
+      new_wrap = line.gsub /(.{1,#{size}})( |$\n?)/, "\\1\n"
+      lengths = new_wrap.split("\n").map(&:size)
+      break if has_meta? and is_last and lengths.last == lengths.max and lengths.count(lengths.max) == 1
+      break if new_wrap.count("\n") > max_height
+      best_wrap = new_wrap
+    end
+    best_wrap.chomp
   end
 
 end end
