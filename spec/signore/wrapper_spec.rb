4019364a3a2a1c57198adea83bfcecb38707b67f
Piotr Szotkowski <shot@hot.pl>
2009-10-16 21:28:08 +0200

    minor æsthetic fix

--- spec/signore/wrapper_spec.rb
+++ spec/signore/wrapper_spec.rb
@@ -1,7 +1,7 @@
 module Signore describe Wrapper do
 
   it 'should wrap signatures properly' do
-    samples = YAML.load File.read 'spec/fixtures/wrapping.yaml'
+    samples = YAML.load_file 'spec/fixtures/wrapping.yaml'
     samples.each do |sample|
       Wrapper.new(sample[:text], sample[:meta]).display.should == sample[:wrapped]
     end
