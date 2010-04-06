e4b267041a0357baac45f25643192dae23250886
Piotr Szotkowski <shot@hot.pl>
2010-04-07 01:19:35 +0200

    Signature.find: handle properly tagless signatures

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -3,8 +3,8 @@ module Signore class Signature < Struct.new :text, :tags
   def self.find opts = {}
     opts = {:tags => [], :no_tags => []}.merge opts
     sigs = Signore.db.shuffle
-    sigs.reject! { |sig| (sig.tags & opts[:tags]).empty? } unless opts[:tags].empty?
-    sigs.reject! { |sig| not (sig.tags & opts[:no_tags]).empty? }
+    sigs.reject! { |sig| not sig.tags or (sig.tags & opts[:tags]).empty? } unless opts[:tags].empty?
+    sigs.reject! { |sig| sig.tags and not (sig.tags & opts[:no_tags]).empty? }
     sigs.first
   end
 
