ffacd72384fddfc4c287a3329c65da773df74928
Piotr Szotkowski <shot@hot.pl>
2010-04-07 00:13:27 +0200

    teach Signature.find to handle tags

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -1,7 +1,12 @@
-module Signore class Signature < Struct.new :text
+module Signore class Signature < Struct.new :text, :tags
 
-  def self.find
-    Signore.db.sort_by { rand }.first
+  def self.find opts = {}
+    if opts[:tags]
+      sigs = Signore.db.reject { |sig| (sig.tags & opts[:tags]).empty? }
+    else
+      sigs = Signore.db
+    end
+    sigs.sort_by { rand }.first
   end
 
 end end
--- spec/signore/signature_spec.rb
+++ spec/signore/signature_spec.rb
@@ -4,12 +4,21 @@ module Signore describe Signature do
 
   context '.find' do
 
-    it 'returns a random signature by default' do
+    before do
+      srand
       Signore.load_db 'spec/fixtures/signatures.yml'
+    end
+
+    it 'returns a random signature by default' do
       srand 1981
       Signature.find.text.should == '// sometimes I believe compiler ignores all my comments'
       srand 1979
       Signature.find.text.should == 'You do have to be mad to work here, but it doesn’t help.'
+    end
+
+    it 'returns a random signature based on provided tags' do
+      Signature.find(:tags => ['programming']).text.should == '// sometimes I believe compiler ignores all my comments'
+      Signature.find(:tags => ['work']).text.should        == 'You do have to be mad to work here, but it doesn’t help.'
     end
 
   end
