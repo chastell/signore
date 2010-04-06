dad921fd14b37a87d3e93f783ae33bdff015d088
Piotr Szotkowski <shot@hot.pl>
2010-04-07 00:21:29 +0200

    refactor Signature.find

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -1,11 +1,8 @@
 module Signore class Signature < Struct.new :text, :tags
 
   def self.find opts = {:tags => []}
-    if opts[:tags].empty?
-      sigs = Signore.db
-    else
-      sigs = Signore.db.reject { |sig| (sig.tags & opts[:tags]).empty? }
-    end
+    sigs = Signore.db.dup
+    sigs.reject! { |sig| (sig.tags & opts[:tags]).empty? } unless opts[:tags].empty?
     sigs.sort_by { rand }.first
   end
 
