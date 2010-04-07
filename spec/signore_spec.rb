19fc4d5d8ae005d3eb3b385110f2577ace729129
Piotr Szotkowski <shot@hot.pl>
2010-04-07 03:20:06 +0200

    make the Signore spec clean up after itself

--- spec/signore_spec.rb
+++ spec/signore_spec.rb
@@ -1,10 +1,17 @@
 describe Signore do
 
+  before do
+    @db = "#{Dir.tmpdir}/#{rand}/signatures.yml"
+  end
+
+  after do
+    FileUtils.rmtree File.dirname @db
+  end
+
   context '.db' do
 
     it 'returns the signature database loaded by .load_db' do
-      db = "#{Dir.tmpdir}/#{rand}/signatures.yml"
-      Signore.load_db db
+      Signore.load_db @db
       Signore.db.should == []
     end
 
@@ -13,10 +20,9 @@ describe Signore do
   context '.load_db' do
 
     it 'creates an empty signature database if it does not exist' do
-      db = "#{Dir.tmpdir}/#{rand}/signatures.yml"
-      File.exists?(db).should be_false
-      Signore.load_db db
-      File.read(db).should == [].to_yaml
+      File.exists?(@db).should be_false
+      Signore.load_db @db
+      File.read(@db).should == [].to_yaml
     end
 
   end
