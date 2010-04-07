cdb68d5e3b89ce3935b7f85d1a83a7d18072e73a
Piotr Szotkowski <shot@hot.pl>
2010-04-07 03:08:05 +0200

    refactor Signature.find

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -2,10 +2,10 @@ module Signore class Signature < Struct.new :text, :author, :source, :subject, :
 
   def self.find opts = {}
     opts = {:tags => [], :no_tags => []}.merge opts
-    sigs = Signore.db.shuffle
-    sigs.reject! { |sig| not sig.tags or (sig.tags & opts[:tags]).empty? } unless opts[:tags].empty?
-    sigs.reject! { |sig| sig.tags and not (sig.tags & opts[:no_tags]).empty? }
-    sigs.first
+    Signore.db
+      .select { |sig| opts[:tags].all?    { |tag| sig.tagged_with? tag } }
+      .reject { |sig| opts[:no_tags].any? { |tag| sig.tagged_with? tag } }
+      .shuffle.first
   end
 
   def display
