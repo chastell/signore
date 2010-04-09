160bf07578560be1bda9cccba582d20c330ad484
Piotr Szotkowski <shot@hot.pl>
2010-04-10 00:47:44 +0200

    structure a bit Executable#run spec

--- spec/signore/executable_spec.rb
+++ spec/signore/executable_spec.rb
@@ -53,45 +53,53 @@ module Signore describe Executable do
 
   context '#run' do
 
-    before do
-      @path = "#{Dir.tmpdir}/#{rand}/signatures.yml"
+    context 'prego' do
+
+      it 'prints a signature tagged with the provided tags' do
+        sig = mock Signature, :display => '// sometimes I believe compiler ignores all my comments'
+        Database.should_receive(:find).with({:tags => ['tech', 'programming'], :no_tags => []}).and_return sig
+        Executable.new(['prego', 'tech', 'programming']).run output = StringIO.new
+        output.rewind
+        output.read.should == "// sometimes I believe compiler ignores all my comments\n"
+      end
+
+      it 'prints a signature based on allowed and forbidden tags' do
+        sig = mock Signature, :display => 'You do have to be mad to work here, but it doesn’t help.'
+        Database.should_receive(:find).with({:tags => ['tech'], :no_tags => ['programming', 'security']}).and_return sig
+        Executable.new(['prego', '~programming', 'tech', '~security']).run output = StringIO.new
+        output.rewind
+        output.read.should == "You do have to be mad to work here, but it doesn’t help.\n"
+      end
+
     end
 
-    after do
-      FileUtils.rmtree File.dirname @path
-    end
+    context 'pronto' do
 
-    it 'prints a signature tagged with the provided tags' do
-      sig = mock Signature, :display => '// sometimes I believe compiler ignores all my comments'
-      Database.should_receive(:find).with({:tags => ['tech', 'programming'], :no_tags => []}).and_return sig
-      Executable.new(['prego', 'tech', 'programming']).run output = StringIO.new
-      output.rewind
-      output.read.should == "// sometimes I believe compiler ignores all my comments\n"
-    end
+      before do
+        @path = "#{Dir.tmpdir}/#{rand}/signatures.yml"
+      end
 
-    it 'prints a signature based on allowed and forbidden tags' do
-      sig = mock Signature, :display => 'You do have to be mad to work here, but it doesn’t help.'
-      Database.should_receive(:find).with({:tags => ['tech'], :no_tags => ['programming', 'security']}).and_return sig
-      Executable.new(['prego', '~programming', 'tech', '~security']).run output = StringIO.new
-      output.rewind
-      output.read.should == "You do have to be mad to work here, but it doesn’t help.\n"
-    end
+      after do
+        FileUtils.rmtree File.dirname @path
+      end
 
-    it 'asks about signature parts and saves given signature with provided labels' do
-      input = StringIO.new "The Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n\nMark Pilgrim\n\n\n\n"
-      Executable.new(['-d', @path, 'pronto', 'Wikipedia', 'ADHD']).run output = StringIO.new, input
-      output.rewind
-      output.read.should == "text?\nauthor?\nsubject?\nsource?\nThe Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n                                                      [Mark Pilgrim]\n"
-      Executable.new(['-d', @path, 'prego', 'Wikipedia', 'ADHD']).run output = StringIO.new
-      output.rewind
-      output.read.should == "The Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n                                                      [Mark Pilgrim]\n"
-    end
+      it 'asks about signature parts and saves given signature with provided labels' do
+        input = StringIO.new "The Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n\nMark Pilgrim\n\n\n\n"
+        Executable.new(['-d', @path, 'pronto', 'Wikipedia', 'ADHD']).run output = StringIO.new, input
+        output.rewind
+        output.read.should == "text?\nauthor?\nsubject?\nsource?\nThe Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n                                                      [Mark Pilgrim]\n"
+        Executable.new(['-d', @path, 'prego', 'Wikipedia', 'ADHD']).run output = StringIO.new
+        output.rewind
+        output.read.should == "The Wikipedia page on ADHD is like 20 pages long. That’s just cruel.\n                                                      [Mark Pilgrim]\n"
+      end
 
-    it 'handles multi-line signatures' do
-      input = StringIO.new "‘I’ve gone through over-stressed to physical exhaustion – what’s next?’\n‘Tuesday.’\n\nSimon Burr, Kyle Hearn\n\n\n\n"
-      Executable.new(['-d', @path, 'pronto']).run output = StringIO.new, input
-      output.rewind
-      output.read.should == "text?\nauthor?\nsubject?\nsource?\n‘I’ve gone through over-stressed to physical exhaustion – what’s next?’\n‘Tuesday.’\n                                               [Simon Burr, Kyle Hearn]\n"
+      it 'handles multi-line signatures' do
+        input = StringIO.new "‘I’ve gone through over-stressed to physical exhaustion – what’s next?’\n‘Tuesday.’\n\nSimon Burr, Kyle Hearn\n\n\n\n"
+        Executable.new(['-d', @path, 'pronto']).run output = StringIO.new, input
+        output.rewind
+        output.read.should == "text?\nauthor?\nsubject?\nsource?\n‘I’ve gone through over-stressed to physical exhaustion – what’s next?’\n‘Tuesday.’\n                                               [Simon Burr, Kyle Hearn]\n"
+      end
+
     end
 
   end
