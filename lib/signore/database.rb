c094d445ec70ef35ecc9843b2b33dcc6d119182d
Piotr Szotkowski <shot@hot.pl>
2010-05-05 22:32:31 -0700

    make Database.yamlify quote comment-like content

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -43,8 +43,8 @@ module Signore class Database
 
   def self.yamlify string
     case string
-    when /\n/ then "|-\n" + string.gsub(/^/, '    ')
-    when /: / then "'#{string.gsub "'", "''"}'"
+    when /\n/       then "|-\n" + string.gsub(/^/, '    ')
+    when /: /, / #/ then "'#{string.gsub "'", "''"}'"
     else string
     end
   end
