bc80d632c0073b6e16aa11f22e8db41d4a88235c
Piotr Szotkowski <shot@hot.pl>
2010-05-12 04:50:05 -0700

    teach Database.yamlify to quote #-starting strings

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -43,8 +43,8 @@ module Signore class Database
 
   def self.yamlify string
     case string
-    when /\n/                                 then "|-\n" + string.gsub(/^ +/) { |s| ' ' * s.size }.gsub(/^/, '    ')
-    when /: /, /^\s*\d*:\d*\s*$/, / #/, /^\s/ then "'#{string.gsub "'", "''"}'"
+    when /\n/                                      then "|-\n" + string.gsub(/^ +/) { |s| ' ' * s.size }.gsub(/^/, '    ')
+    when /: /, /^\s*\d*:\d*\s*$/, /(^|\s)#/, /^\s/ then "'#{string.gsub "'", "''"}'"
     else string
     end
   end
