3aec0fcb06faf621f989f3f0019324ce1c08a1bf
Piotr Szotkowski <chastell@chastell.net>
2010-08-24 02:08:17 +0200

    Psychosis

--- lib/signore.rb
+++ lib/signore.rb
@@ -2,6 +2,8 @@ require 'fileutils'
 require 'trollop'
 require 'yaml'
 
+YAML::ENGINE.yamler = 'psych' if defined? Psych
+
 require_relative 'signore/database'
 require_relative 'signore/executable'
 require_relative 'signore/signature'
