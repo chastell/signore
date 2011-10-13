fecb945b2ab084bcde9b8921467755375ef1d173
Piotr Szotkowski <chastell@chastell.net>
2011-10-13 22:59:12 +0200

    switch back from autoload

--- lib/signore.rb
+++ lib/signore.rb
@@ -4,9 +4,7 @@ require 'yaml'
 
 YAML::ENGINE.yamler = 'psych' if defined? Psych
 
-module Signore
-  autoload :Database,   'signore/database'
-  autoload :Executable, 'signore/executable'
-  autoload :Signature,  'signore/signature'
-  autoload :Wrapper,    'signore/wrapper'
-end
+require_relative 'signore/database'
+require_relative 'signore/executable'
+require_relative 'signore/signature'
+require_relative 'signore/wrapper'
