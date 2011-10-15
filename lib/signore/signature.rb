d4b0df046fe486169556feae242b5d575af26cf9
Piotr Szotkowski <chastell@chastell.net>
2011-10-15 04:02:54 +0200

    factor out Signature#add_meta_to

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -6,17 +6,17 @@ module Signore class Signature < Struct.new :text, :author, :source, :subject, :
 
   def to_s
     lines = text.split("\n").map { |line| LovelyRufus::Wrapper.new(line).wrapped 80 }
-
-    if meta
-      lines << "[#{meta}]"
-      max = lines.map { |line| line.split "\n" }.flatten.map(&:size).max
-      lines.last.insert 0, ' ' * (max - meta.size - 2)
-    end
-
+    add_meta_to lines if meta
     lines.join "\n"
   end
 
   private
+
+  def add_meta_to lines
+    lines << "[#{meta}]"
+    max = lines.map { |line| line.split "\n" }.flatten.map(&:size).max
+    lines.last.insert 0, ' ' * (max - meta.size - 2)
+  end
 
   def meta
     case
