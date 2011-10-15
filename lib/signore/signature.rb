69cc920c6f6d0d6a66c03fe1c9ad8237aba9ac74
Piotr Szotkowski <chastell@chastell.net>
2011-10-15 04:32:45 +0200

    refactor Signature#to_s

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -5,18 +5,11 @@ module Signore class Signature < Struct.new :text, :author, :source, :subject, :
   end
 
   def to_s
-    lines = text.split("\n").map { |line| LovelyRufus::Wrapper.new(line).wrapped 80 }
-    add_meta_to lines if meta
-    lines.join "\n"
+    wrapped = LovelyRufus::Wrapper.new(text.gsub("\n", "\n\n")).wrapped(80).gsub("\n\n", "\n")
+    wrapped + meta_for(wrapped)
   end
 
   private
-
-  def add_meta_to lines
-    lines << "[#{meta}]"
-    max = lines.map { |line| line.split "\n" }.flatten.map(&:size).max
-    lines.last.insert 0, ' ' * (max - meta.size - 2)
-  end
 
   def meta
     case
@@ -27,6 +20,15 @@ module Signore class Signature < Struct.new :text, :author, :source, :subject, :
     when source                      then "#{source}"
     when subject                     then "#{subject}"
     end
+  end
+
+  def meta_for wrapped
+    return '' unless meta
+
+    indent = wrapped.split("\n").map(&:size).max - meta.size - 2
+    spaces = indent > 0 ? ' ' * indent : ''
+
+    "\n#{spaces}[#{meta}]"
   end
 
 end end
