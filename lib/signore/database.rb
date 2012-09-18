44b6f94b3a036d572b31b9fc6f7ecdf22859a491
Piotr Szotkowski <chastell@chastell.net>
2012-09-18 12:20:25 +0200

    Database: attr_readers > ivar access

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -1,27 +1,29 @@
 # encoding: UTF-8
 
 module Signore class Database
-
   def initialize path
     @store = YAML::Store.new path
   end
 
   def << sig
-    @store.transaction do
-      @store['signatures'] ||= []
-      @store['signatures'] << sig
+    store.transaction do
+      store['signatures'] ||= []
+      store['signatures'] << sig
     end
   end
 
   def find opts = {}
     opts = { tags: [], no_tags: [] }.merge opts
 
-    @store.transaction true do
-      @store['signatures']
+    store.transaction true do
+      store['signatures']
         .select { |sig| opts[:tags].all?    { |tag| sig.tagged_with? tag } }
         .reject { |sig| opts[:no_tags].any? { |tag| sig.tagged_with? tag } }
         .shuffle.first
     end
   end
 
+  private
+
+  attr_reader :store
 end end
