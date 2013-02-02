ef9c1ac82c150bda3e35881bbc5334b13aee34ee
Piotr Szotkowski <chastell@chastell.net>
2013-02-02 14:01:15 +0100

    refactor Signature spec a bit more

--- spec/signore/signature_spec.rb
+++ spec/signore/signature_spec.rb
@@ -3,44 +3,37 @@
 require_relative '../spec_helper'
 
 module Signore describe Signature do
-  let(:compiler) do
-    Signature.new '// sometimes I believe compiler ignores all my comments',
-      nil, nil, nil, ['programming', 'tech']
-  end
-
-  let(:dads) do
-    Signature.new 'stay-at-home executives vs. wallstreet dads', 'kodz'
-  end
-
   describe '#tagged_with?' do
     it 'says whether a tagged signature is tagged with a given tag' do
-      refute compiler.tagged_with? 'fnord'
-      assert compiler.tagged_with? 'programming'
-      assert compiler.tagged_with? 'tech'
+      sig = Signature.new nil, nil, nil, nil, ['programming', 'tech']
+      refute sig.tagged_with? 'fnord'
+      assert sig.tagged_with? 'programming'
+      assert sig.tagged_with? 'tech'
     end
 
     it 'says that an untagged signature is not tagged with any tag' do
-      refute dads.tagged_with? 'fnord'
+      refute Signature.new.tagged_with? 'fnord'
     end
   end
 
   describe '#to_s' do
     it 'does not show meta if there’s nothing to show' do
-      compiler.to_s.must_equal <<-end.dedent.strip
+      sig = Signature.new '// sometimes I believe compiler ignores all my comments'
+      sig.to_s.must_equal <<-end.dedent.strip
         // sometimes I believe compiler ignores all my comments
       end
     end
 
     it 'shows author on its own' do
-      dads.to_s.must_equal <<-end.dedent.strip
+      sig = Signature.new 'stay-at-home executives vs. wallstreet dads', 'kodz'
+      sig.to_s.must_equal <<-end.dedent.strip
         stay-at-home executives vs. wallstreet dads
                                              [kodz]
       end
     end
 
     it 'shows author and source, comma-separated' do
-      sig = Signature.new 'You do have to be mad to work here, but it doesn’t help.',
-        'Gary Barnes', 'asr'
+      sig = Signature.new 'You do have to be mad to work here, but it doesn’t help.', 'Gary Barnes', 'asr'
       sig.to_s.must_equal <<-end.dedent.strip
         You do have to be mad to work here, but it doesn’t help.
                                               [Gary Barnes, asr]
@@ -48,8 +41,7 @@ module Signore describe Signature do
     end
 
     it 'shows source on its own' do
-      sig = Signature.new 'Bruce Schneier knows Alice and Bob’s shared secret.',
-        nil, 'Bruce Schneier Facts'
+      sig = Signature.new 'Bruce Schneier knows Alice and Bob’s shared secret.', nil, 'Bruce Schneier Facts'
       sig.to_s.must_equal <<-end.dedent.strip
         Bruce Schneier knows Alice and Bob’s shared secret.
                                      [Bruce Schneier Facts]
@@ -57,8 +49,9 @@ module Signore describe Signature do
     end
 
     it 'shows author and subject, space separated' do
-      sig = Signature.new 'She was good at playing abstract confusion in the same way a midget is good at being short.',
-        'Clive James', nil, 'on Marilyn Monroe'
+      sig = Signature.new(
+        'She was good at playing abstract confusion in the same way a midget is good at being short.',
+        'Clive James', nil, 'on Marilyn Monroe')
       sig.to_s.must_equal <<-end.dedent.strip
         She was good at playing abstract confusion in
         the same way a midget is good at being short.
@@ -67,8 +60,9 @@ module Signore describe Signature do
     end
 
     it 'shows subject on its own' do
-      sig = Signature.new 'Amateur fighter pilot ignores orders, listens to the voices in his head and slaughters thousands.',
-        nil, nil, 'Star Wars ending explained'
+      sig = Signature.new(
+        'Amateur fighter pilot ignores orders, listens to the voices in his head and slaughters thousands.',
+        nil, nil, 'Star Wars ending explained')
       sig.to_s.must_equal <<-end.dedent.strip
         Amateur fighter pilot ignores orders, listens to
         the voices in his head and slaughters thousands.
