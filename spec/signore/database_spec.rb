4ef29a3a1e14be3cf4117d45cacde7a88a62b5ea
Piotr Szotkowski <chastell@chastell.net>
2013-10-02 10:14:26 +0200

    Database spec: wrap long lines

--- spec/signore/database_spec.rb
+++ spec/signore/database_spec.rb
@@ -25,17 +25,19 @@ module Signore describe Database do
     end
 
     it 'returns a random signature based on required and forbidden tags' do
-      db.find(required_tags: ['tech'], forbidden_tags: %w[programming security]).text
+      forbidden_tags = %w[programming security]
+      db.find(required_tags: ['tech'], forbidden_tags: forbidden_tags).text
         .must_equal 'You do have to be mad to work here, but it doesn’t help.'
     end
   end
 
   describe '#<<' do
     it 'saves the provided signature to disk' do
+      text = 'Normaliser Unix c’est comme pasteuriser le camembert.'
       file = Tempfile.new ''
       db   = Database.new file.path
-      db  << Signature.new('Normaliser Unix c’est comme pasteuriser le camembert.')
-      file.read.must_include 'Normaliser Unix c’est comme pasteuriser le camembert.'
+      db  << Signature.new(text)
+      file.read.must_include text
     end
   end
 end end
