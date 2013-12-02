e639b64ecaacc5f432357469c2086a98f8a9a0ca
Piotr Szotkowski <chastell@chastell.net>
2013-12-02 09:33:51 +0100

    bring back the crazy indentation

--- lib/signore/signature.rb
+++ lib/signore/signature.rb
@@ -1,41 +1,39 @@
-module Signore
-  Signature = Struct.new :text, :author, :source, :subject, :tags do
-    class << self
-      undef :[]
-    end
-
-    def self.[](author: nil, source: nil, subject: nil, tags: nil, text: nil)
-      new text, author, source, subject, tags
-    end
-
-    def tagged_with? tag
-      tags and tags.include? tag
-    end
-
-    def to_s
-      wrapper = LovelyRufus::Wrapper.new text.gsub("\n", "\n\n")
-      wrapped = wrapper.wrapped(80).gsub "\n\n", "\n"
-      wrapped + meta_for(wrapped)
-    end
-
-    private
-
-    def indent_size_for text
-      indent = text_width(text) - meta.size - 2
-      indent < 0 ? 0 : indent
-    end
-
-    def meta
-      stem = [author, subject].compact.join ' '
-      stem.empty? ? "#{source}" : [stem, source].compact.join(', ')
-    end
-
-    def meta_for text
-      meta.empty? ? '' : "\n#{' ' * indent_size_for(text)}[#{meta}]"
-    end
-
-    def text_width text
-      text.split("\n").map(&:size).max
-    end
+module Signore Signature = Struct.new(*%i[text author source subject tags]) do
+  class << self
+    undef :[]
   end
-end
+
+  def self.[](author: nil, source: nil, subject: nil, tags: nil, text: nil)
+    new text, author, source, subject, tags
+  end
+
+  def tagged_with? tag
+    tags and tags.include? tag
+  end
+
+  def to_s
+    wrapper = LovelyRufus::Wrapper.new text.gsub("\n", "\n\n")
+    wrapped = wrapper.wrapped(80).gsub "\n\n", "\n"
+    wrapped + meta_for(wrapped)
+  end
+
+  private
+
+  def indent_size_for text
+    indent = text_width(text) - meta.size - 2
+    indent < 0 ? 0 : indent
+  end
+
+  def meta
+    stem = [author, subject].compact.join ' '
+    stem.empty? ? "#{source}" : [stem, source].compact.join(', ')
+  end
+
+  def meta_for text
+    meta.empty? ? '' : "\n#{' ' * indent_size_for(text)}[#{meta}]"
+  end
+
+  def text_width text
+    text.split("\n").map(&:size).max
+  end
+end end
