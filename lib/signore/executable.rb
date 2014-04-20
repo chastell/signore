d408cf0405de3f714d6ef1b67813112d445610fc
Piotr Szotkowski <chastell@chastell.net>
2014-04-20 08:33:33 +0200

    move Executable::Settings to its own file

--- lib/signore/executable.rb
+++ lib/signore/executable.rb
@@ -1,6 +1,6 @@
-require 'optparse'
 require 'ostruct'
 require_relative 'database'
+require_relative 'executable/settings'
 require_relative 'signature'
 
 module Signore class Executable
@@ -24,29 +24,6 @@ module Signore class Executable
   private     :db, :settings
 
   private
-
-  Settings = Struct.new(*%i(action db_path forbidden_tags required_tags)) do
-    def initialize args
-      db_dir = ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
-      self.db_path = "#{db_dir}/signore/signatures.yml"
-      parse_settings args, self
-      self.action = args.shift
-      self.forbidden_tags, self.required_tags = args.partition do |tag|
-        tag.start_with? '~'
-      end
-      forbidden_tags.map! { |tag| tag[1..-1] }
-    end
-
-    private
-
-    def parse_settings args, settings
-      OptionParser.new do |opts|
-        opts.on '-d', '--database PATH', 'Database location' do |path|
-          settings.db_path = path
-        end
-      end.parse! args
-    end
-  end
 
   def get_param param, input
     puts "#{param}?"
