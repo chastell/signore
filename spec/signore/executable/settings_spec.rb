8f792d1207f0fc403f1508b3dc0bcd73849c9597
Piotr Szotkowski <chastell@chastell.net>
2014-04-21 07:41:00 +0200

    spec Executable::Settings#db_path some more

--- spec/signore/executable/settings_spec.rb
+++ spec/signore/executable/settings_spec.rb
@@ -1,15 +1,19 @@
+require 'tmpdir'
 require_relative '../../spec_helper'
 require_relative '../../../lib/signore/executable/settings'
 
 module Signore class Executable; describe Settings do
   describe '#db_path' do
-    it 'defaults to ~/.local/share/signore/signatures.yml' do
+    it 'defaults to $XDG_DATA_HOME/signore/signatures.yml' do
       begin
-        xdg_data_home = ENV.delete 'XDG_DATA_HOME'
+        old_xdg = ENV.delete 'XDG_DATA_HOME'
         default_path = File.expand_path '~/.local/share/signore/signatures.yml'
         Settings.new([]).db_path.must_equal default_path
+        ENV['XDG_DATA_HOME'] = Dir.mktmpdir
+        default_path = "#{ENV['XDG_DATA_HOME']}/signore/signatures.yml"
+        Settings.new([]).db_path.must_equal default_path
       ensure
-        ENV['XDG_DATA_HOME'] = xdg_data_home
+        old_xdg ? ENV['XDG_DATA_HOME'] = old_xdg : ENV.delete('XDG_DATA_HOME')
       end
     end
   end
