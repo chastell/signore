b89ec703b9aa7316cf37c3a901078b42d758e103
Piotr Szotkowski <chastell@chastell.net>
2014-04-25 07:51:13 +0200

    Executable::Settings: defactor before refactoring

--- lib/signore/executable/settings.rb
+++ lib/signore/executable/settings.rb
@@ -3,10 +3,8 @@ require 'optparse'
 module Signore class Executable
   Settings = Struct.new(*%i(action db_path forbidden_tags required_tags)) do
     def initialize args
-      db_dir = ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
-      self.db_path = "#{db_dir}/signore/signatures.yml"
-      parse_settings args, self
-      self.action = args.shift
+      self.db_path = get_db_path_from args
+      self.action  = args.shift
       self.forbidden_tags, self.required_tags = args.partition do |tag|
         tag.start_with? '~'
       end
@@ -15,12 +13,18 @@ module Signore class Executable
 
     private
 
-    def parse_settings args, settings
+    def db_dir
+      ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
+    end
+
+    def get_db_path_from args
+      db_path = "#{db_dir}/signore/signatures.yml"
       OptionParser.new do |opts|
         opts.on '-d', '--database PATH', 'Database location' do |path|
-          settings.db_path = path
+          db_path = path
         end
       end.parse! args
+      db_path
     end
   end
 end end
