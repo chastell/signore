fa5e4d6560bec24a28aad2da6f24022722110895
Piotr Szotkowski <chastell@chastell.net>
2014-04-28 06:42:25 +0200

    Executable::Settings: switch away from a Struct

--- lib/signore/executable/settings.rb
+++ lib/signore/executable/settings.rb
@@ -1,30 +1,30 @@
 require 'optparse'
 
-module Signore class Executable
-  Settings = Struct.new(*%i(action db_path forbidden_tags required_tags)) do
-    def initialize args
-      self.db_path = get_db_path_from args
-      self.action  = args.shift
-      self.forbidden_tags, self.required_tags = args.partition do |tag|
-        tag.start_with? '~'
-      end
-      forbidden_tags.map! { |tag| tag[1..-1] }
-    end
+module Signore class Executable; class Settings
+  attr_reader :action, :db_path, :forbidden_tags, :required_tags
 
-    private
-
-    def db_dir
-      ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
+  def initialize args
+    @db_path = get_db_path_from args
+    @action  = args.shift
+    @forbidden_tags, @required_tags = args.partition do |tag|
+      tag.start_with? '~'
     end
-
-    def get_db_path_from args
-      db_path = "#{db_dir}/signore/signatures.yml"
-      OptionParser.new do |opts|
-        opts.on '-d', '--database PATH', 'Database location' do |path|
-          db_path = path
-        end
-      end.parse! args
-      db_path
-    end
+    forbidden_tags.map! { |tag| tag[1..-1] }
   end
-end end
+
+  private
+
+  def db_dir
+    ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
+  end
+
+  def get_db_path_from args
+    db_path = "#{db_dir}/signore/signatures.yml"
+    OptionParser.new do |opts|
+      opts.on '-d', '--database PATH', 'Database location' do |path|
+        db_path = path
+      end
+    end.parse! args
+    db_path
+  end
+end end end
