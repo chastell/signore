efb8d8acaa50c65980e99ad798c4d16e1f92cfb4
Piotr Szotkowski <chastell@chastell.net>
2014-05-11 06:00:08 +0200

    factor out Database#find_random_among

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -14,13 +14,22 @@ module Signore class Database
 
   def find forbidden_tags: [], random: Random.new, required_tags: []
     store.transaction true do
-      store['signatures']
-        .select { |sig| required_tags.all?  { |tag| sig.tagged_with? tag } }
-        .reject { |sig| forbidden_tags.any? { |tag| sig.tagged_with? tag } }
-        .sample random: random
+      find_random_among store['signatures'], forbidden_tags: forbidden_tags,
+                                             random:         random,
+                                             required_tags:  required_tags
     end
   end
 
   attr_reader :store
   private     :store
+
+  private
+
+  def find_random_among sigs, forbidden_tags: forbidden_tags, random: random,
+                        required_tags: required_tags
+    sigs
+      .select { |sig| required_tags.all?  { |tag| sig.tagged_with? tag } }
+      .reject { |sig| forbidden_tags.any? { |tag| sig.tagged_with? tag } }
+      .sample random: random
+  end
 end end
