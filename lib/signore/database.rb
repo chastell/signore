704d5e45982e10862287427d14d64b678643c9ea
Piotr Szotkowski <chastell@chastell.net>
2014-05-13 00:14:14 +0200

    Database: pass randomness source to the constructor

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -1,8 +1,9 @@
 require 'yaml/store'
 
 module Signore class Database
-  def initialize path
-    @store = YAML::Store.new path
+  def initialize path, random: Random.new
+    @random = random
+    @store  = YAML::Store.new path
   end
 
   def << sig
@@ -12,21 +13,19 @@ module Signore class Database
     end
   end
 
-  def find forbidden_tags: [], random: Random.new, required_tags: []
+  def find forbidden_tags: [], required_tags: []
     store.transaction true do
       find_random_among store['signatures'], forbidden_tags: forbidden_tags,
-                                             random:         random,
                                              required_tags:  required_tags
     end
   end
 
-  attr_reader :store
-  private     :store
+  attr_reader :random, :store
+  private     :random, :store
 
   private
 
-  def find_random_among sigs, forbidden_tags: forbidden_tags, random: random,
-                        required_tags: required_tags
+  def find_random_among sigs, forbidden_tags: [], required_tags: []
     sigs
       .select { |sig| required_tags.all?  { |tag| sig.tagged_with? tag } }
       .reject { |sig| forbidden_tags.any? { |tag| sig.tagged_with? tag } }
