280b5bdddee951b3cb73b4a052bcdc720cc479fc
Piotr Szotkowski <chastell@chastell.net>
2014-05-14 08:30:11 +0200

    start factoring out Database::SigFinder

--- lib/signore/database.rb
+++ lib/signore/database.rb
@@ -15,20 +15,28 @@ module Signore class Database
 
   def find forbidden_tags: [], required_tags: []
     store.transaction true do
-      find_random_among store['signatures'], forbidden_tags: forbidden_tags,
-                                             required_tags:  required_tags
+      SigFinder.new(random).find_random store['signatures'],
+                                        forbidden_tags: forbidden_tags,
+                                        required_tags:  required_tags
     end
   end
 
   attr_reader :random, :store
   private     :random, :store
 
-  private
+  class SigFinder
+    def initialize random = Random.new
+      @random = random
+    end
 
-  def find_random_among sigs, forbidden_tags: [], required_tags: []
-    sigs
-      .select { |sig| required_tags.all?  { |tag| sig.tagged_with? tag } }
-      .reject { |sig| forbidden_tags.any? { |tag| sig.tagged_with? tag } }
-      .sample random: random
+    def find_random sigs, forbidden_tags: [], required_tags: []
+      sigs
+        .select { |sig| required_tags.all?  { |tag| sig.tagged_with? tag } }
+        .reject { |sig| forbidden_tags.any? { |tag| sig.tagged_with? tag } }
+        .sample random: random
+    end
+
+    attr_reader :random
+    private     :random
   end
 end end
