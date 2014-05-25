bda2f3accaf28a012b59cde12890ce154be50dda
Piotr Szotkowski <chastell@chastell.net>
2014-05-25 10:42:03 +0200

    spec SigFinder a bit more

--- spec/signore/sig_finder_spec.rb
+++ spec/signore/sig_finder_spec.rb
@@ -3,14 +3,23 @@ require_relative '../spec_helper'
 require_relative '../../lib/signore/sig_finder'
 
 module Signore describe SigFinder do
+  let :sigs do
+    store = YAML::Store.new 'spec/fixtures/signatures.yml'
+    store.transaction(true) { store['signatures'] }
+  end
+
   describe '#find_tagged' do
     it 'returns a random Signature by default' do
-      store = YAML::Store.new 'spec/fixtures/signatures.yml'
-      sigs  = store.transaction(true) { store['signatures'] }
       SigFinder.new(sigs, random: Random.new(1981)).find_tagged.text
         .must_include 'Amateur fighter pilot ignores orders'
       SigFinder.new(sigs, random: Random.new(2009)).find_tagged.text
         .must_include '// sometimes I believe compiler ignores all my comments'
+    end
+
+    it 'returns a random signature if the tags are empty' do
+      SigFinder.new(sigs, random: Random.new(2013))
+        .find_tagged(forbidden_tags: [], required_tags: []).text
+        .must_equal '// sometimes I believe compiler ignores all my comments'
     end
   end
 end end
