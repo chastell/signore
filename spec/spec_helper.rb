3fbbe98bed1a94894656130fc5c23735146ed25b
Piotr Szotkowski <chastell@chastell.net>
2014-05-31 11:06:09 +0200

    spec_helper: use Bogus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bogus/minitest/spec'
 require 'signore'
+
+Bogus.configure { |config| config.search_modules << Signore }
 
 class String
   def dedent
