dcbd5c6095f8c46abf2e274a1d3e5c7b8902a3bd
Piotr Szotkowski <chastell@chastell.net>
2014-06-22 01:34:59 +0100

    SigFromStream spec: factor out output example

--- spec/signore/sig_from_stream_spec.rb
+++ spec/signore/sig_from_stream_spec.rb
@@ -4,6 +4,16 @@ require_relative '../../lib/signore/sig_from_stream'
 
 module Signore describe SigFromStream do
   describe '.sig_from' do
+    it 'asks about signature parts' do
+      input = StringIO.new "\n\n\n\n"
+      capture_io { SigFromStream.sig_from input }.first.must_equal <<-end.dedent
+        text?
+        author?
+        subject?
+        source?
+      end
+    end
+
     it 'asks about signature parts and returns resulting signature' do
       input = StringIO.new <<-end.dedent
         You do have to be mad to work here, but it doesn’t help.
@@ -16,14 +26,7 @@ module Signore describe SigFromStream do
       end
 
       sig = nil
-      capture_io do
-        sig = SigFromStream.sig_from input
-      end.first.must_equal <<-end.dedent
-        text?
-        author?
-        subject?
-        source?
-      end
+      capture_io { sig = SigFromStream.sig_from input }
       sig.must_equal Signature.new 'You do have to be mad to work here, ' \
         'but it doesn’t help.', 'Gary Barnes', 'asr', '', []
     end
@@ -37,14 +40,7 @@ module Signore describe SigFromStream do
       end
 
       sig = nil
-      capture_io do
-        sig = SigFromStream.sig_from input
-      end.first.must_equal <<-end.dedent
-        text?
-        author?
-        subject?
-        source?
-      end
+      capture_io { sig = SigFromStream.sig_from input }
       sig.must_equal Signature.new '‘You’ve got an interesting accent. ' \
         "Subtle. I can’t place it.’\n‘It’s text-to-speech… " \
         'I was raised by smartphones.’', 'Patrick Ewing', '', '', []
