decd2a703e99400785c104c0e107f97042fe565b
Piotr Szotkowski <chastell@chastell.net>
2014-08-14 17:59:08 +0200

    oops, make Settings.new work without arguments

--- lib/signore/settings.rb
+++ lib/signore/settings.rb
@@ -5,7 +5,7 @@ module Signore
   class Settings
     attr_reader :db_path
 
-    def initialize(args)
+    def initialize(args = [])
       @args  = args
       db_dir = ENV.fetch('XDG_DATA_HOME') { File.expand_path '~/.local/share' }
       @db_path = "#{db_dir}/signore/signatures.yml"
