a9b1288b3ed1a962c91f1f3e25b1d6f9a26164f4
Piotr Szotkowski <chastell@chastell.net>
2014-11-05 17:59:10 +0100

    add Mapper.from_h

--- lib/signore/mapper.rb
+++ lib/signore/mapper.rb
@@ -1,6 +1,12 @@
+require_relative 'signature'
+
 module Signore
   module Mapper
     module_function
+
+    def from_h(hash)
+      Signature.new(hash.map { |key, value| [key.to_sym, value] }.to_h)
+    end
 
     def to_h(signature)
       signature.to_h
--- spec/signore/mapper_spec.rb
+++ spec/signore/mapper_spec.rb
@@ -4,6 +4,23 @@ require_relative '../../lib/signore/signature'
 
 module Signore
   describe Mapper do
+    describe '.from_h' do
+      it 'deserializes a Signature from a Hash' do
+        text = 'For the sake of topic titles, I’d rather if Monty saved Python.'
+        hash = {
+          'author'  => 'Anonymous Coward',
+          'source'  => '/.',
+          'subject' => 'on ‘Monty Wants to Save MySQL’',
+          'tags'    => %w(/. MySQL),
+          'text'    => text,
+        }
+        sig = Signature.new(author: 'Anonymous Coward', source: '/.',
+                            subject: 'on ‘Monty Wants to Save MySQL’',
+                            tags: %w(/. MySQL), text: text)
+        Mapper.from_h(hash).must_equal sig
+      end
+    end
+
     describe '.to_h' do
       it 'serialises a Signature to a Hash' do
         text = 'For the sake of topic titles, I’d rather if Monty saved Python.'
