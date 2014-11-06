7adb6858995131633dc567944d38c06ed568838a
Piotr Szotkowski <chastell@chastell.net>
2014-11-06 06:22:17 +0100

    refactor Mapper spec

--- spec/signore/mapper_spec.rb
+++ spec/signore/mapper_spec.rb
@@ -4,36 +4,33 @@ require_relative '../../lib/signore/signature'
 
 module Signore
   describe Mapper do
+    let(:sig_hash) do
+      {
+        'author'  => 'Anonymous Coward',
+        'source'  => '/.',
+        'subject' => 'on ‘Monty Wants to Save MySQL’',
+        'tags'    => %w(/. MySQL),
+        'text'    => text,
+      }
+    end
+    let(:signature) do
+      Signature.new(author: 'Anonymous Coward', source: '/.',
+                    subject: 'on ‘Monty Wants to Save MySQL’',
+                    tags: %w(/. MySQL), text: text)
+    end
+    let(:text) do
+      'For the sake of topic titles, I’d rather if Monty saved Python.'
+    end
+
     describe '.from_h' do
       it 'deserializes a Signature from a Hash' do
-        text = 'For the sake of topic titles, I’d rather if Monty saved Python.'
-        hash = {
-          'author'  => 'Anonymous Coward',
-          'source'  => '/.',
-          'subject' => 'on ‘Monty Wants to Save MySQL’',
-          'tags'    => %w(/. MySQL),
-          'text'    => text,
-        }
-        sig = Signature.new(author: 'Anonymous Coward', source: '/.',
-                            subject: 'on ‘Monty Wants to Save MySQL’',
-                            tags: %w(/. MySQL), text: text)
-        Mapper.from_h(hash).must_equal sig
+        Mapper.from_h(sig_hash).must_equal signature
       end
     end
 
     describe '.to_h' do
       it 'serialises a Signature to a Hash' do
-        text = 'For the sake of topic titles, I’d rather if Monty saved Python.'
-        sig = Signature.new(author: 'Anonymous Coward', source: '/.',
-                            subject: 'on ‘Monty Wants to Save MySQL’',
-                            tags: %w(/. MySQL), text: text)
-        Mapper.to_h(sig).must_equal(
-          'author'  => 'Anonymous Coward',
-          'source'  => '/.',
-          'subject' => 'on ‘Monty Wants to Save MySQL’',
-          'tags'    => %w(/. MySQL),
-          'text'    => text,
-        )
+        Mapper.to_h(signature).must_equal sig_hash
       end
     end
   end
