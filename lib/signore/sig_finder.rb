7206ef8787d4586e14bd3034f14e6e927e62ca10
Piotr Szotkowski <chastell@chastell.net>
2015-05-06 00:11:31 +0200

    SigFinder: inline everything to .find

--- lib/signore/sig_finder.rb
+++ lib/signore/sig_finder.rb
@@ -4,17 +4,8 @@ require_relative 'tags'
 module Signore
   class SigFinder
     def self.find(sigs, random: Random.new, tags: Tags.new)
-      new(sigs, random: random).find_tagged(tags: tags)
+      found = sigs.shuffle(random: random).find { |sig| tags.match?(sig.tags) }
+      found or Signature.new
     end
-
-    def initialize(sigs, random: Random.new)
-      @sigs = sigs.shuffle(random: random)
-    end
-
-    def find_tagged(tags: Tags.new)
-      sigs.find { |sig| tags.match?(sig.tags) } or Signature.new
-    end
-
-    private_attr_reader :sigs
   end
 end
