170c0550bef86b7dbc3c72022d34b7403b26fe6a
Piotr Szotkowski <chastell@chastell.net>
2015-05-07 06:32:47 +0200

    turn SigFinder into a module

--- lib/signore/sig_finder.rb
+++ lib/signore/sig_finder.rb
@@ -2,8 +2,10 @@ require_relative 'signature'
 require_relative 'tags'
 
 module Signore
-  class SigFinder
-    def self.find(sigs, random: Random.new, tags: Tags.new)
+  module SigFinder
+    module_function
+
+    def find(sigs, random: Random.new, tags: Tags.new)
       found = sigs.shuffle(random: random).find { |sig| tags.match?(sig.tags) }
       found or Signature.new
     end
