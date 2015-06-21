864033d7474c7fa248d86134e7b1d1914a7016de
Piotr Szotkowski <chastell@chastell.net>
2015-06-22 00:26:39 +0200

    port SigFromStream test to Expectation syntax

--- test/signore/sig_from_stream_test.rb
+++ test/signore/sig_from_stream_test.rb
@@ -6,10 +6,8 @@ module Signore
   describe SigFromStream do
     describe '.sig_from' do
       it 'asks about signature parts' do
-        input = StringIO.new("\n\n\n\n")
-        capture_io do
-          SigFromStream.sig_from input
-        end.first.must_equal <<-end.dedent
+        io = capture_io { SigFromStream.sig_from StringIO.new("\n\n\n\n") }
+        _(io.first).must_equal <<-end.dedent
           text?
           author?
           subject?
@@ -30,8 +28,8 @@ module Signore
         sig = nil
         capture_io { sig = SigFromStream.sig_from input }
         text = 'You do have to be mad to work here, but it doesn’t help.'
-        sig.must_equal Signature.new(author: 'Gary Barnes', source: 'asr',
-                                     text: text)
+        _(sig).must_equal Signature.new(author: 'Gary Barnes', source: 'asr',
+                                        text: text)
       end
 
       it 'handles multi-line signatures' do
@@ -48,7 +46,7 @@ module Signore
           ‘You’ve got an interesting accent. Subtle. I can’t place it.’
           ‘It’s text-to-speech… I was raised by smartphones.’
         end
-        sig.must_equal Signature.new(author: 'Patrick Ewing', text: text)
+        _(sig).must_equal Signature.new(author: 'Patrick Ewing', text: text)
       end
     end
   end
