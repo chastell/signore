231bc7f3b83d8130b2e637da9beba551640186ce
Piotr Szotkowski <chastell@chastell.net>
2017-10-28 00:07:28 +0200

    Bump Repo#default_path to a class method

--- lib/signore/repo.rb
+++ lib/signore/repo.rb
@@ -8,7 +8,11 @@ module Signore
   class Repo
     extend Forwardable
 
-    def initialize(path: default_path)
+    def self.default_path
+      Settings.new.repo_path
+    end
+
+    def initialize(path: self.class.default_path)
       @path = path
       convert if legacy?
     end
@@ -29,11 +33,6 @@ module Signore
 
     def convert
       self.hashes = hashes.map(&:to_h)
-    end
-
-    # :reek:UtilityFunction
-    def default_path
-      Settings.new.repo_path
     end
 
     def legacy?
