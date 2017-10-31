9943d3981d001e2a4a533b9a46a80e279534fcd8
Piotr Szotkowski <chastell@chastell.net>
2017-10-31 05:29:39 +0100

    Drop Settings#repo_path

--- lib/signore/settings.rb
+++ lib/signore/settings.rb
@@ -11,12 +11,6 @@ module Signore
       args.first
     end
 
-    # :reek:UtilityFunction
-    def repo_path
-      dir = ENV.fetch('XDG_DATA_HOME') { File.expand_path('~/.local/share') }
-      Pathname.new("#{dir}/signore/signatures.yml")
-    end
-
     def tags
       negated, required = tag_names.partition { |name| name.start_with?('~') }
       Tags.new(forbidden: negated.map { |neg| neg[1..-1] }, required: required)
